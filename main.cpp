7ff2a3bb2c0e8ec0fe21e0c3d00cbe7d8e866b53
TheRoD2k <47718223+TheRoD2k@users.noreply.github.com>
2019-12-20 18:21:42 +0300

    Merged functions

--- main.cpp
+++ main.cpp
@@ -2,37 +2,34 @@
 #include <string>
 #include <vector>
 
-std::vector<int> ClassicalPrefixFunction(std::string &str) {
-	std::vector<int> prefix_vector(str.size(), 0);
-	for (int i = 1; i < str.size(); i++) {
-		int temp = prefix_vector[i - 1];
-		while ((temp > 0) && (str[i] != str[temp]))
-			temp = prefix_vector[temp - 1];
-		if (str[i] == str[temp])
-			++temp;
-		prefix_vector[i] = temp;
-	}
-	return prefix_vector;
-}
-
-bool CheckIfSubstringPrefixFunction(std::string& pattern, std::vector<int>& prefix_vector, char input) {
-	static int last_prefix = 0, current_prefix = 0;
-	current_prefix = last_prefix;
+void SearchSymbol(std::vector<int>& prefix_vector, std::string& pattern, char& input, int& current_prefix, int& last_prefix ) {
 	while ((current_prefix > 0) && (pattern[current_prefix] != input)) {
 		current_prefix = prefix_vector[current_prefix - 1];
 	}
 	if (pattern[current_prefix] == input)
 		++current_prefix;
 	last_prefix = current_prefix;
-	return (last_prefix == pattern.size() ? true : false);
 }
+
+std::vector<int> ClassicalPrefixFunction(std::string& pattern) {
+	std::vector<int> prefix_vector(pattern.size(), 0);
+	for (int i = 1; i < pattern.size(); i++) {
+		int current_prefix = prefix_vector[i - 1];
+		SearchSymbol(prefix_vector, pattern, pattern[i], current_prefix, prefix_vector[i]);
+	}
+	return prefix_vector;
+}
+
 
 void ReadLineAndPrintSubstringIndexes(std::string& pattern) {
 	std::vector<int> prefix_vector = ClassicalPrefixFunction(pattern);
 	char input;
 	int pos = 0;
+	int last_prefix = 0, current_prefix = 0;
+	current_prefix = last_prefix;
 	while (std::cin >> input) {
-		if (CheckIfSubstringPrefixFunction(pattern, prefix_vector, input))
+		SearchSymbol(prefix_vector, pattern, input, current_prefix, last_prefix);
+		if (last_prefix == pattern.size())
 			std::cout << pos - pattern.size() + 1 << " ";
 		++pos;
 	}
